b26431b554b541b9396ca7acd0e41ec72f7b0f4a
aa1422-dotcom <aronlenin97@gmail.com>
2026-07-07 04:00:19 -0500

    Update SecurityConfig.java

--- src/main/java/com/laserranita/backend/config/SecurityConfig.java
+++ src/main/java/com/laserranita/backend/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.laserranita.backend.config;
 
+import java.util.Arrays;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
